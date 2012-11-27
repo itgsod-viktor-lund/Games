bf43bc24ffd9e6c5c70ca3e4697c865163b9ac6c
Viktor Lund <viktor.lund@elev.it-gymnasiet.se>
2012-11-27 15:44:05 +0100

    Added background, shooting and walls
    In this update I added a background image, walls and the ability to
    shoot.
    I also fixed the bug on the player.

--- Dungeon Hunter/dungeon_hunter_game.rb
+++ Dungeon Hunter/dungeon_hunter_game.rb
@@ -6,7 +6,25 @@ class Game < Chingu::Window
 		super 800, 550
 		self.caption = "Dungeon Hunter"
 		self.input = {esc: :exit}
+		@background_image = Background.create
+		@wall_image = Wall.create
 		@player = Player.create
+	end
+end
+
+class Background < Chingu::GameObject
+
+	def setup
+		@x, @y = 800/2, 550/2
+		@image = Gosu::Image["background.png"]
+	end
+end
+
+class Wall < Chingu::GameObject
+
+	def setup
+		@x, @y = 800/2, 550/2
+		@image = Gosu::Image["wall.png"]
 	end
 end
 
@@ -14,38 +32,72 @@ class Player < Chingu::GameObject
 	has_traits :velocity
 	
 	def setup
-		@x, @y = 800/2, 550/2
-		@player_image = Gosu::Image["player.png"]
+		@lock = false
+		@x, @y = 100, 100
+		@angle_shot = 0
+		@image = Gosu::Image["player.png"]
 
 		self.input = {
 			holding_up: :up,
 			holding_down: :down,
 			holding_right: :right,
-			holding_left: :left
+			holding_left: :left,
+			space: :fire
 		}
 	end
 
 	def update
-		@x = 800
-		@y = 550
+		@lock = false
+	end
+
+	def lock_check
+		if @lock == false
+			@lock = true
+		end
 	end
 
 	def up
-		self.velocity_y -= 5
+		if lock_check
+			@y -= 5
+			@angle_shot = 0
+		end
 	end
 
 	def down
-		self.velocity_y += 5
+		if lock_check
+			@y += 5
+			@angle_shot = 180
+		end
 	end
 
 	def right
-		self.velocity_x += 5
+		if lock_check
+			@x += 5
+			@angle_shot = 90
+		end
 	end
 
 	def left
-		self.velocity_x -= 5
+		if lock_check
+			@x -= 5
+			@angle_shot = 270
+		end
 	end
 
+	def fire
+		Laser.create(x: self.x, y: self.y, angle: @angle_shot)
+	end
+end
+
+class Laser < Chingu::GameObject
+	has_traits :velocity, :timer, :collision_detection, :bounding_circle
+
+	def setup
+		@image = Gosu::Image["flame_ball.png"]
+		self.velocity_x = Gosu::offset_y(angle, 10)
+		self.velocity_y = Gosu::offset_y(angle, 10)
+		after(3000) {self.destroy}
+	end
 end
 
 Game.new.show
